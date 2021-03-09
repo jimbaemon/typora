6cc12618d488a543d01529d750fb8249e7e9145a
jimbaemon <jimbae@kakao.com>
2021-03-09 21:19:17 +0900

    FIX : APACHE HTTPCLIENT 에 맞춰서 내용 변경

--- client/src/main/java/typora/image/cilent/service/ImageUploadService.java
+++ client/src/main/java/typora/image/cilent/service/ImageUploadService.java
@@ -1,33 +1,28 @@
 package typora.image.cilent.service;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.http.HttpClient;
 import java.nio.file.Files;
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ImageUploadService {
-
-    private final HttpClient httpClient = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_2)
-            .connectTimeout(Duration.ofSeconds(20))
-            .build();
 
     public String imageUpload(String filePath) throws IOException {
         CloseableHttpClient client = HttpClients.createDefault();
 
         HttpPost post = new HttpPost("http://www.jimbae.com:59005/image");
-        HttpPost post = new HttpPost("http://127.0.0.1:59005/image");
         File file = new File(filePath);
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
         builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
